d29486abff51a771ba5e287a295d551507c52b08
bentzion hadad <bentzion9@gmail.com>
2025-04-20 18:07:38 +0300

    more getters

--- dev/Domain/DiscountDomain.java
+++ dev/Domain/DiscountDomain.java
@@ -6,4 +6,16 @@ public class DiscountDomain {
     private int percent;
     private Date discountEnd;
     private Date discountStart;
+
+
+    public int getpercent() { return percent;}
+    public Date getdiscountStart() { return discountStart;}
+    public Date getdiscountEnd() { return discountEnd;}
+
+    public DiscountService(int disPercent, Date sDate, Date eDate){
+        percent = disPercent;
+        discountStart = sDate;
+        discountEnd = eDate;
+    }
+
 }
--- dev/Domain/ProductDomain.java
+++ dev/Domain/ProductDomain.java
@@ -12,4 +12,25 @@ public class ProductDomain {
     private String wareHouseShelf;
     private String storeShelf;
     private List<SupplyDomain> supplyList; //// list of supplies ////
+
+
+    public int getproductID() { return productID;}
+    public String getproductName() { return productName;}
+    public String getmanufactuerName() { return manufactuerName;}
+    public int getminimalAmountStore() { return minimalAmountStore;}
+    public int getminimalAmountStock() { return minimalAmountStock;}
+    public float getproductPrice1unit() { return productPrice1unit;}
+    public String getstoreShelf() { return storeShelf;}
+    public String getwareHouseShelf() { return wareHouseShelf;}
+
+    public ProductDomain(int pID,String pName,String MfName, int MAStore, int MAStock, float PPrice,String SShalf,String WHShelf){
+        productID = pID;
+        productName = pName;
+        manufactuerName = MfName;
+        minimalAmountStore = MAStore;
+        minimalAmountStock = MAStock;
+        productPrice1unit = PPrice;
+        storeShelf = SShalf;
+        wareHouseShelf = WHShelf;
+
 }
