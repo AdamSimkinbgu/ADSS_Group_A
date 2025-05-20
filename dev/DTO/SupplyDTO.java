70f88e03b0e9c3a621e40268c4f54c819e1f9fe8
bentzion hadad <bentzion9@gmail.com>
2025-05-20 17:36:59 +0300

    Dto changes

--- dev/DTO/SupplyDTO.java
+++ dev/DTO/SupplyDTO.java
@@ -1,4 +1,36 @@
 package DTO;
 
+import Domain.SupplyDomain;
+
 public class SupplyDTO {
+
+    int productID;
+    LocalDate expireDate;
+    int quantity;
+
+
+    public int getProductID() {
+        return productID;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public LocalDate getExpireDate() {
+        return expireDate;
+    }
+
+    public SupplyDTO(){}
+
+    public SupplyDTO(int id, int quantity, LocalDate ex){
+        productID = id;
+        quantity = quantity;
+        expireDate = ex;
+    }
+    public SupplyDTO(SupplyDomain other , int otherId){
+        productID = otherId;
+        quantity = other.getQuantityBad();
+        expireDate = other.getExpierDate();
+    }
+
+
 }
