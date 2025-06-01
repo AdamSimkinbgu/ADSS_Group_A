680087b479ab6c9547ddf4a8ccd6517521da6016
Adam Simkin <simkinad@post.bgu.ac.il>
2025-06-01 11:23:29 +0300

    added OrderPackageDTO for Inventory use
    object that represents an order that was delivered and awaits inventory handling, later will help with completing the order (changing its status to "Completed").

---
+++ dev/Suppliers/DTOs/OrderPackageDTO.java
@@ -0,0 +1,84 @@
+package Suppliers.DTOs;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import Inventory.DTO.SupplyDTO;
+
+public class OrderPackageDTO {
+   private String orderId;
+   private LocalDate deliveryDate;
+   private List<SupplyDTO> supplies; // having of products with their quantities, productId and their experation
+                                     // dates
+
+   public OrderPackageDTO(String orderId, LocalDate deliveryDate, List<SupplyDTO> supplies) {
+      this.orderId = orderId;
+      this.deliveryDate = deliveryDate;
+      this.supplies = supplies;
+   }
+
+   public String getOrderId() {
+      return orderId;
+   }
+
+   public void setOrderId(String orderId) {
+      this.orderId = orderId;
+   }
+
+   public LocalDate getDeliveryDate() {
+      return deliveryDate;
+   }
+
+   public void setDeliveryDate(LocalDate deliveryDate) {
+      this.deliveryDate = deliveryDate;
+   }
+
+   public List<SupplyDTO> getSupplies() {
+      return supplies;
+   }
+
+   public void setSupplies(List<SupplyDTO> supplies) {
+      this.supplies = supplies;
+   }
+
+   @Override
+   public String toString() {
+      StringBuilder sb = new StringBuilder();
+      sb.append("{\n");
+      sb.append("   \"orderId\": \"").append(orderId).append("\",\n");
+      sb.append("   \"deliveryDate\": \"").append(deliveryDate).append("\",\n");
+      sb.append("   \"supplies\": [\n");
+      for (int i = 0; i < supplies.size(); i++) {
+         sb.append("      ").append(supplies.get(i).toString());
+         if (i < supplies.size() - 1) {
+            sb.append(",\n");
+         } else {
+            sb.append("\n");
+         }
+      }
+      sb.append("   ]\n");
+      sb.append("}");
+      return sb.toString();
+   }
+
+   @Override
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (!(o instanceof OrderPackageDTO))
+         return false;
+      OrderPackageDTO that = (OrderPackageDTO) o;
+      return orderId.equals(that.orderId) &&
+            deliveryDate.equals(that.deliveryDate) &&
+            supplies.equals(that.supplies);
+   }
+
+   @Override
+   public int hashCode() {
+      int result = orderId.hashCode();
+      result = 31 * result + deliveryDate.hashCode();
+      result = 31 * result + supplies.hashCode();
+      return result;
+   }
+
+}
