3eb47b39d779d300434b0c0f98ca544ed7d5ec90
Adam Simkin <simkinad@post.bgu.ac.il>
2025-06-02 21:05:09 +0300

    Update database operations and repositories
    - Implement JDBC operations for agreements
    - Add Bill of Quantities database handling
    - Update supplier data access methods
    - Modify repository implementation

--- dev/Suppliers/DataLayer/util/Database.java
+++ dev/Suppliers/DataLayer/util/Database.java
@@ -44,6 +44,36 @@ public final class Database {
                                 UNIQUE(tax_number)
                             );
                         """);
+                // create a trigger to detect deletions and adjust the supplier_id accordingly
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_supplier_autoincrement
+                // AFTER INSERT ON suppliers
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE suppliers
+                // SET supplier_id = (
+                // COALESCE(
+                // (SELECT MAX(supplier_id) + 1 FROM suppliers),
+                // 1
+                // )
+                // )
+                // WHERE rowid = NEW.rowid; -- patch only the row we just inserted
+                // END;
+                // """);
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_supplier_reseq_after_delete
+                // AFTER DELETE ON suppliers
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE suppliers
+                // SET supplier_id = supplier_id - 1
+                // WHERE supplier_id > OLD.supplier_id;
+                // END;
+                // """);
+                // st.executeUpdate("""
+                // CREATE INDEX IF NOT EXISTS idx_suppliers_tax_number
+                // ON suppliers(tax_number);
+                // """);
 
                 /* ───────────────────────── contacts ───────────────────────── */
                 st.executeUpdate("""
@@ -56,6 +86,7 @@ public final class Database {
                                 UNIQUE (supplier_id, phone),
                                 FOREIGN KEY(supplier_id) REFERENCES suppliers(supplier_id)
                                     ON DELETE CASCADE
+                                    ON UPDATE CASCADE
                             );
                         """);
 
@@ -72,12 +103,39 @@ public final class Database {
                                 days_to_expiry         INTEGER NOT NULL CHECK(days_to_expiry >= 0),
                                 FOREIGN KEY(supplier_id) REFERENCES suppliers(supplier_id)
                                     ON DELETE CASCADE
+                                    ON UPDATE CASCADE
                             );
                         """);
-                st.executeUpdate("""
-                            CREATE INDEX IF NOT EXISTS idx_supplier_products_supplier
-                            ON supplier_products(supplier_id);
-                        """);
+                // st.executeUpdate("""
+                // CREATE INDEX IF NOT EXISTS idx_supplier_products_supplier
+                // ON supplier_products(supplier_id);
+                // """);
+                // // create a trigger to detect deletions and adjust the product_id accordingly
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_product_autoincrement
+                // AFTER INSERT ON supplier_products
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE supplier_products
+                // SET product_id = (
+                // COALESCE(
+                // (SELECT MAX(product_id) + 1 FROM supplier_products),
+                // 1
+                // )
+                // )
+                // WHERE rowid = NEW.rowid; -- patch only the row we just inserted
+                // END;
+                // """);
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_product_reseq_after_delete
+                // AFTER DELETE ON supplier_products
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE supplier_products
+                // SET product_id = product_id - 1
+                // WHERE product_id > OLD.product_id;
+                // END;
+                // """);
 
                 /* ───────────────────────── agreements ──────────────────────── */
                 st.executeUpdate("""
@@ -86,29 +144,60 @@ public final class Database {
                                 supplier_id           INTEGER NOT NULL,
                                 agreement_start_date  TEXT    NOT NULL,
                                 agreement_end_date    TEXT    NOT NULL,
-                                has_fixed_supply_days INTEGER NOT NULL
-                                                     CHECK(has_fixed_supply_days IN (0,1)),
                                 valid                 INTEGER NOT NULL DEFAULT 1
                                                      CHECK(valid IN (0,1)),
                                 CHECK(agreement_end_date >= agreement_start_date),
                                 FOREIGN KEY(supplier_id) REFERENCES suppliers(supplier_id)
                                     ON DELETE CASCADE
+                                    ON UPDATE CASCADE
                             );
                         """);
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_agreement_autoincrement
+                // AFTER INSERT ON agreements
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE agreements
+                // SET agreement_id = (
+                // COALESCE(
+                // (SELECT MAX(agreement_id) + 1 FROM agreements),
+                // 1
+                // )
+                // )
+                // WHERE rowid = NEW.rowid; -- patch only the row we just inserted
+                // END;
+                // """);
+                // st.executeUpdate("""
+                // CREATE TRIGGER IF NOT EXISTS trg_agreement_reseq_after_delete
+                // AFTER DELETE ON agreements
+                // FOR EACH ROW
+                // BEGIN
+                // UPDATE agreements
+                // SET agreement_id = agreement_id - 1
+                // WHERE agreement_id > OLD.agreement_id;
+                // END;
+                // """);
+                // st.executeUpdate("""
+                // CREATE INDEX IF NOT EXISTS idx_agreements_supplier
+                // ON agreements(supplier_id);
+                // """);
 
                 /* ───────────────────────── boq_items ───────────────────────── */
                 st.executeUpdate("""
                             CREATE TABLE IF NOT EXISTS boq_items(
                                 agreement_id     INTEGER NOT NULL,
-                                line_in_bill     INTEGER NOT NULL CHECK(line_in_bill > 0),
+                                line_in_bill     INTEGER,
                                 product_id       INTEGER NOT NULL,
                                 quantity         INTEGER NOT NULL CHECK(quantity > 0),
                                 discount_percent REAL    NOT NULL
                                                 CHECK(discount_percent BETWEEN 0 AND 100),
                                 PRIMARY KEY (agreement_id, line_in_bill),
                                 FOREIGN KEY(agreement_id) REFERENCES agreements(agreement_id)
-                                    ON DELETE CASCADE,
+                                    ON DELETE CASCADE
+                                    ON UPDATE CASCADE,
                                 FOREIGN KEY(product_id)   REFERENCES supplier_products(product_id)
+                                    ON DELETE CASCADE
+                                    ON UPDATE CASCADE
                             );
                         """);
                 st.executeUpdate("""
@@ -156,6 +245,8 @@ public final class Database {
             st.executeUpdate("DELETE FROM supplier_products;");
             st.executeUpdate("DELETE FROM contacts;");
             st.executeUpdate("DELETE FROM suppliers;");
+            st.executeUpdate("DELETE FROM sqlite_sequence;"); // reset autoincrement
+            // counters
             log.info("All data deleted from the database");
         } catch (SQLException e) {
             log.error("Failed to delete all data", e);
