7b67f371c732e0fc01a4d503df842f85eb9735fe
Adam Simkin <simkinad@post.bgu.ac.il>
2025-07-08 21:02:24 +0300

    Update ScreensEnum to include suppliers navigation path

--- dev/PresentationLayer/GUI/Common/Navigation/ScreensEnum.java
+++ dev/PresentationLayer/GUI/Common/Navigation/ScreensEnum.java
@@ -4,9 +4,15 @@ public enum ScreensEnum {
    LOGIN("/GUI/LoginScreen/Views/LoginView.fxml"),
    MENU("/GUI/MainMenuScreen/Views/MainMenuView.fxml"),
    EMPLOYEE("/GUI/EmployeeScreen/Views/MainView.fxml"),
-   SUPPLIERS("/GUI/SupplierScreen/Views/SuppliersView.fxml"),
+   SUPPLIERS("/GUI/SupplierScreen/Views/SuppliersMainView.fxml"),
    INVENTORY("/GUI/InventoryScreen/Views/InventoryView.fxml"),
-   SHIPMENTS("/GUI/TransportationScreen/Views/ShipmentsView.fxml");
+   SHIPMENTS("/GUI/TransportationScreen/Views/ShipmentsView.fxml"),
+   SUPPLIERS_DASHBOARD("/GUI/SupplierScreen/Views/SuppliersDashboardView.fxml"),
+   SUPPLIERS_LIST("/GUI/SupplierScreen/Views/SuppliersListView.fxml"),
+   SUPPLIER_DETAILS("/GUI/SupplierScreen/Views/SupplierDetailsView.fxml"),
+   SUPPLIER_EDIT("/GUI/SupplierScreen/Views/SupplierEditView.fxml"),
+   SUPPLIER_ADD("/GUI/SupplierScreen/Views/SupplierAddView.fxml"),
+   SUPPLIER_REMOVE("/GUI/SupplierScreen/Views/SupplierRemoveView.fxml");
 
    private final String fxmlPath;
 
